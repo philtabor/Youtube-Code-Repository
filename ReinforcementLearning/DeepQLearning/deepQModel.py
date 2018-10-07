7ecfaa754cdf3d64095644ea6a660f85526ac1de
MachineLearningLab-AI <ptabor@gmail.com>
2018-10-07 16:14:18 -0700

    split up network from agent class and added target network replacement

--- ReinforcementLearning/DeepQLearning/deepQModel.py
+++ ReinforcementLearning/DeepQLearning/deepQModel.py
@@ -4,18 +4,9 @@ import torch.nn.functional as F
 import torch.optim as optim
 import numpy as np
 
-class deepQNetwork(nn.Module):
-    def __init__(self, gamma, epsilon, alpha, 
-                 maxMemorySize, actionSpace=[0,1,2,3,4,5]):
-        super(deepQNetwork, self).__init__()
-        self.GAMMA = gamma
-        self.EPSILON = epsilon
-        self.ALPHA = alpha
-        self.actionSpace = actionSpace
-        self.memSize = maxMemorySize
-        self.steps = 0
-        self.memory = []
-        self.memCntr = 0
+class DeepQNetwork(nn.Module):
+    def __init__(self, ALPHA):
+        super(DeepQNetwork, self).__init__()
         #self.conv1 = nn.Conv2d(3, 32, 8, stride=4, padding=1)
         self.conv1 = nn.Conv2d(1, 32, 8, stride=4, padding=1)
         self.conv2 = nn.Conv2d(32, 64, 4, stride=2)
@@ -24,10 +15,10 @@ class deepQNetwork(nn.Module):
         self.fc1 = nn.Linear(128*19*8, 512)
         self.fc2 = nn.Linear(512, 6)
         #self.optimizer = optim.SGD(self.parameters(), lr=self.ALPHA, momentum=0.9)
-        self.optimizer = optim.RMSprop(self.parameters(), lr=self.ALPHA)
+        self.optimizer = optim.RMSprop(self.parameters(), lr=ALPHA)
         self.loss = nn.MSELoss()
         self.device = T.device('cuda:0' if T.cuda.is_available() else 'cpu')     
-        self.to(self.device)        
+        self.to(self.device)
 
     def forward(self, observation):
         observation = T.Tensor(observation).to(self.device)        
@@ -42,6 +33,24 @@ class deepQNetwork(nn.Module):
         actions = self.fc2(observation).to(self.device)        
         return actions
 
+class Agent(object):
+    def __init__(self, gamma, epsilon, alpha, 
+                 maxMemorySize, epsEnd=0.05, 
+                 replace=10000, actionSpace=[0,1,2,3,4,5]):
+        self.GAMMA = gamma
+        self.EPSILON = epsilon
+        self.EPS_END = epsEnd
+        self.ALPHA = alpha
+        self.actionSpace = actionSpace
+        self.memSize = maxMemorySize
+        self.steps = 0
+        self.learn_step_counter = 0
+        self.memory = []
+        self.memCntr = 0
+        self.replace_target_cnt = replace
+        self.Q_eval = DeepQNetwork(alpha)
+        self.Q_next = DeepQNetwork(alpha)
+
     def storeTransition(self, state, action, reward, state_):
         if self.memCntr < self.memSize:
             self.memory.append([state, action, reward, state_])
@@ -51,16 +60,21 @@ class deepQNetwork(nn.Module):
         
     def chooseAction(self, observation):
         rand = np.random.random()
-        actions = self.forward(observation)
+        actions = self.Q_eval.forward(observation)
         if rand < 1 - self.EPSILON:
-            action = T.argmax(actions[1]).item()            
+            action = T.argmax(actions[1]).item()
         else:
-            action = np.random.choice(self.actionSpace)
+            action = np.random.choice(self.actionSpace)            
         self.steps += 1
         return action
     
-    def learn(self, batch_size):
-        self.optimizer.zero_grad()
+    def learn(self, batch_size, episode):
+        self.Q_eval.optimizer.zero_grad()
+        if self.replace_target_cnt is not None and \
+           self.learn_step_counter % self.replace_target_cnt == 0:
+            print('replacing target network at episode', episode)
+            self.Q_next.load_state_dict(self.Q_eval.state_dict())
+
         if self.memCntr+batch_size < self.memSize:            
             memStart = int(np.random.choice(range(self.memCntr-batch_size-1)))
         else:
@@ -68,21 +82,22 @@ class deepQNetwork(nn.Module):
         miniBatch=self.memory[memStart:memStart+batch_size]
         memory = np.array(miniBatch)
 
-        Qpred = self.forward(list(memory[:,0][:])).to(self.device)
-        Qnext = self.forward(list(memory[:,3][:])).to(self.device)
+        Qpred = self.Q_eval.forward(list(memory[:,0][:])).to(self.Q_eval.device)
+        Qnext = self.Q_next.forward(list(memory[:,3][:])).to(self.Q_eval.device)
 
-        maxA = T.argmax(Qnext, dim=1).to(self.device) 
+        maxA = T.argmax(Qnext, dim=1).to(self.Q_eval.device) 
         Qtarget = Qpred
-        rewards = T.Tensor(list(memory[:,2])).to(self.device)        
+        rewards = T.Tensor(list(memory[:,2])).to(self.Q_eval.device)        
         Qtarget[:,maxA] = rewards + self.GAMMA*T.max(Qnext[1])
         
-        if self.steps > 25000:
-            if self.EPSILON - 5e-6 > 0.05:
-                self.EPSILON -= 5e-6
+        if self.steps > 500:
+            if self.EPSILON - 1e-4 > self.EPS_END:
+                self.EPSILON -= 1e-4
             else:
-                self.EPSILON = 0
+                self.EPSILON = self.EPS_END
 
         Qpred.requires_grad_()
-        loss = self.loss(Qtarget, Qpred).to(self.device)
+        loss = self.Q_eval.loss(Qtarget, Qpred).to(self.Q_eval.device)
         loss.backward()
-        self.optimizer.step()       
+        self.Q_eval.optimizer.step()
+        self.learn_step_counter += 1
