2898562732c4fcc19eef74a9eaedcd052bb05a42
philtabor <ptabor@gmail.com>
2019-04-28 22:01:33 -0700

    solve space invaders with policy gradients

--- ReinforcementLearning/PolicyGradient/reinforce/cnn_model.py
+++ ReinforcementLearning/PolicyGradient/reinforce/cnn_model.py
@@ -1,10 +1,12 @@
 import tensorflow as tf
 import numpy as np
+import os
 
 class PolicyGradientAgent(object):
-    def __init__(self, ALPHA, GAMMA=0.95, n_actions=4, 
-                 input_shape=(185,95), channels=1):
-        
+    def __init__(self, ALPHA, GAMMA=0.95, n_actions=4, fc1=256,
+                 input_shape=(185,95), channels=1,  chkpt_dir='tmp/checkpoints',
+                 gpu={'GPU':1}):
+
         self.lr = ALPHA
         self.gamma = GAMMA
         self.n_actions = n_actions
@@ -15,64 +17,76 @@ class PolicyGradientAgent(object):
         self.state_memory = []
         self.action_memory = []
         self.reward_memory = []
-        self.sess = tf.Session()
+        self.gpu = gpu
+        self.fc1 = fc1
+        config = tf.ConfigProto(device_count = self.gpu)
+        self.sess = tf.Session(config=config)
         self.build_net()
         self.sess.run(tf.global_variables_initializer())
-    
+        self.saver = tf.train.Saver()
+        self.checkpoint_file = os.path.join(chkpt_dir,'policy_network.ckpt')
+
     def build_net(self):
         with tf.variable_scope('parameters'):
-            self.input = tf.placeholder(tf.float32, 
-                    shape=[None, self.input_height, self.input_width, self.channels], 
+            self.input = tf.placeholder(tf.float32,
+                    shape=[None, self.input_height, self.input_width,
+                           self.channels],
                     name='input')
-            self.label = tf.placeholder(tf.int32, 
-                                    shape=[None, ], name='label')
+            self.label = tf.placeholder(tf.int32,
+                                    shape=[None], name='label')
             self.G = tf.placeholder(tf.float32, shape=[None,], name='G')
 
         with tf.variable_scope('conv_layer_1'):
-            conv1 = tf.layers.conv2d(inputs=self.input, filters=32, kernel_size=(8,8), 
-                                     strides=4, name='conv1')
-            batch1 = tf.layers.batch_normalization(inputs=conv1, epsilon=1e-5, 
+            conv1 = tf.layers.conv2d(inputs=self.input, filters=32,
+                                     kernel_size=(8,8), strides=4, name='conv1',
+                      kernel_initializer=tf.contrib.layers.xavier_initializer())
+            batch1 = tf.layers.batch_normalization(inputs=conv1, epsilon=1e-5,
                                                    name='batch1')
-            conv1_activated = tf.nn.elu(batch1)
+            conv1_activated = tf.nn.relu(batch1)
 
         with tf.variable_scope('conv_layer_2'):
-            conv2 = tf.layers.conv2d(inputs=conv1_activated, filters=64, 
-                                     kernel_size=(4,4), strides=2, name='conv2')
-            batch2 = tf.layers.batch_normalization(inputs=conv2, epsilon=1e-5, 
+            conv2 = tf.layers.conv2d(inputs=conv1_activated, filters=64,
+                                     kernel_size=(4,4), strides=2, name='conv2',
+                      kernel_initializer=tf.contrib.layers.xavier_initializer())
+            batch2 = tf.layers.batch_normalization(inputs=conv2, epsilon=1e-5,
                                                    name='batch2')
-            conv2_activated = tf.nn.elu(batch2)
+            conv2_activated = tf.nn.relu(batch2)
 
         with tf.variable_scope('conv_layer_3'):
-            conv3 = tf.layers.conv2d(inputs=conv2_activated, filters=128, 
-                                     kernel_size=(3,3),strides=1, name='conv3')
+            conv3 = tf.layers.conv2d(inputs=conv2_activated, filters=128,
+                                     kernel_size=(3,3),strides=1, name='conv3',
+                      kernel_initializer=tf.contrib.layers.xavier_initializer())
             batch3 = tf.layers.batch_normalization(inputs=conv3, epsilon=1e-5)
-            conv3_activated = tf.nn.elu(batch3)
+            conv3_activated = tf.nn.relu(batch3)
 
         with tf.variable_scope('fc1'):
             flat = tf.layers.flatten(conv3_activated)
-            dense1 = tf.layers.dense(flat, units=128)
-        
+            dense1 = tf.layers.dense(flat, units=self.fc1, activation=tf.nn.relu)
+
         with tf.variable_scope('fc2'):
-            dense2 = tf.layers.dense(dense1, units=512)
+            dense2 = tf.layers.dense(dense1, units=self.n_actions,
+                      kernel_initializer=tf.contrib.layers.xavier_initializer())
 
-        with tf.variable_scope('fc3'):
-            dense3 = tf.layers.dense(dense2, units=self.n_actions)
-
-        self.actions = tf.nn.softmax(dense3, name='actions')
+        self.actions = tf.nn.softmax(dense2, name='actions')
 
         with tf.variable_scope('loss'):
             negative_log_probability = tf.nn.sparse_softmax_cross_entropy_with_logits(
-                                                logits=dense3, labels=self.label)            
-            self.loss = negative_log_probability * self.G
-                    
-        with tf.variable_scope('train'):
-            self.train_op = tf.train.RMSPropOptimizer(self.lr).minimize(self.loss) 
+                                                logits=dense2, labels=self.label)
+            self.loss = tf.reduce_mean(negative_log_probability * self.G)
 
-    def choose_action(self, observation): 
-        observation = np.array(observation).reshape(
-                        (-1, self.input_height, self.input_width, self.channels))
-        probabilities = self.sess.run(self.actions, 
+        with tf.variable_scope('train'):
+            # used this one for video
+            self.train_op = tf.train.RMSPropOptimizer(self.lr, decay=0.99,
+                                 momentum=0.0, epsilon=1e-6).minimize(self.loss)
+            #self.train_op = tf.train.AdamOptimizer(self.lr, epsilon=1e-3).minimize(self.loss)
+
+    def choose_action(self, observation):
+        observation = np.array(observation).reshape((-1, self.input_height,
+                                                     self.input_width,
+                                                     self.channels))
+        probabilities = self.sess.run(self.actions,
                                       feed_dict={self.input: observation})[0]
+
         action = np.random.choice(self.action_space, p=probabilities)
 
         return action
@@ -80,28 +94,42 @@ class PolicyGradientAgent(object):
     def store_transition(self, observation, action, reward):
         self.state_memory.append(observation)
         self.action_memory.append(action)
-        self.reward_memory.append(reward)      
-        
+
+        self.reward_memory.append(reward)
+
     def learn(self):
         state_memory = np.array(self.state_memory).reshape(
-                    (-1, self.input_height, self.input_width, self.channels))     
-        action_memory = np.array(self.action_memory)        
+                    (-1, self.input_height, self.input_width, self.channels))
+        action_memory = np.array(self.action_memory)
         reward_memory = np.array(self.reward_memory)
-        
-        values = np.zeros_like(self.reward_memory)
-        G = 0
-        for t in reversed(range(reward_memory.shape[0])):
-            G = self.gamma*G + reward_memory[t]
-            values[t] = G
 
-        mean = np.mean(values)
-        std = np.std(values) if np.std(values) > 0 else 1
-        values = (values - mean) / std
-                
-        _ = self.sess.run(self.train_op, 
+        G = np.zeros_like(reward_memory)
+        for t in range(len(reward_memory)):
+            G_sum = 0
+            discount = 1
+            for k in range(t, len(reward_memory)):
+                if t > 0 and reward_memory[k] - reward_memory[k-1] < 0:
+                    break
+                G_sum += reward_memory[k] * discount
+                discount *= self.gamma
+            G[t] = G_sum
+
+        mean = np.mean(G)
+        std = np.std(G) if np.std(G) > 0 else 1
+        G = (G - mean) / std
+
+        _ = self.sess.run(self.train_op,
                             feed_dict={self.input: state_memory,
                                        self.label: action_memory,
-                                       self.G: values})
+                                       self.G: G})
         self.state_memory = []
         self.action_memory = []
         self.reward_memory = []
+
+    def load_checkpoint(self):
+        print("...Loading checkpoint...")
+        self.saver.restore(self.sess, self.checkpoint_file)
+
+    def save_checkpoint(self):
+        print("...Saving checkpoint...")
+        self.saver.save(self.sess, self.checkpoint_file)
--- ReinforcementLearning/PolicyGradient/reinforce/reinforce_space_invaders.py
+++ ReinforcementLearning/PolicyGradient/reinforce/reinforce_space_invaders.py
@@ -1,38 +1,65 @@
 import numpy as np
 import gym
 from cnn_model import PolicyGradientAgent
-import matplotlib.pyplot as plt
 from utils import plotLearning
+from gym import wrappers
 
-def process_obs(observation):
+
+def preprocess(observation):
     return np.mean(observation[15:200, 30:125], axis=2)
 
+
+def stack_frames(stacked_frames, frame, buffer_size):
+    if stacked_frames is None:
+        stacked_frames = np.zeros((buffer_size, *frame.shape))
+        for idx, _ in enumerate(stacked_frames):
+            stacked_frames[idx,:] = frame
+    else:
+        stacked_frames[0:buffer_size-1,:] = stacked_frames[1:,:]
+        stacked_frames[buffer_size-1, :] = frame
+
+    return stacked_frames
+
 if __name__ == '__main__':
-    agent = PolicyGradientAgent(ALPHA=0.00001, GAMMA=0.95, n_actions=6)
-    env = gym.make('SpaceInvaders-v0')    
+    load_checkpoint = False
+    agent = PolicyGradientAgent(ALPHA=0.001, GAMMA=0.9, n_actions=6, fc1=256,
+                                chkpt_dir='tmp/checkpoint-newG-0p001', gpu={'GPU':1})
+    filename = 'space-invaders-alpha001-newGcalc.png'
+    print('will use ', filename, ' and ', agent.gpu)
+    if load_checkpoint:
+        agent.load_checkpoint()
+    env = gym.make('SpaceInvaders-v0')
     score_history = []
     score = 0
     num_episodes = 1000
-    last_action = 0
+    stack_size = 4
+    #env = wrappers.Monitor(env, "tmp/space-invaders-newG-0p003",
+    #                        video_callable=lambda episode_id: True, force=True)
     for i in range(num_episodes):
-        print('episode: ', i,'score: ', score)
         done = False
-        score = 0        
+
+        avg_score = np.mean(score_history[max(0, i-20):(i+1)])
+        if i % 20 == 0 and i > 0:
+            print('episode: ', i,'score: ', score, ' average score %.3f' % avg_score)
+            plotLearning(score_history, filename=filename, window=20)
+        else:
+            print('episode: ', i,'score: ', score)
         observation = env.reset()
-        frames = [process_obs(observation)]
-        while not done:    
-            if len(frames) == 3:
-                action = agent.choose_action(frames)
-                frames = []
-            else:
-                action = last_action                                                        
-            observation_, reward, done, info = env.step(action)                        
-            agent.store_transition(process_obs(observation), action, reward)            
-            observation = observation_
-            last_action = action
-            frames.append(process_obs(observation))
+        observation = preprocess(observation)
+        stacked_frames = None
+        stacked_frames = stack_frames(stacked_frames, observation, stack_size)
+        score = 0
+        while not done:
+            action = agent.choose_action(stacked_frames)
+            observation, reward, done, info = env.step(action)
+            observation = preprocess(observation)
+            stacked_frames = stack_frames(stacked_frames, observation, stack_size)
+            agent.store_transition(observation, action, reward)
+
             score += reward
-        score_history.append(score)        
-        agent.learn()                
-    filename = 'alpha00001-rewards-space-invaders.png'
+        score_history.append(score)
+
+        if i % 10 == 0:
+            agent.learn()
+            agent.save_checkpoint()
     plotLearning(score_history, filename=filename, window=20)
