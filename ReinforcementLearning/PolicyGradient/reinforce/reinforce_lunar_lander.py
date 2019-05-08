7f568dd3a572bcd5641a45af4d93ccc9893e854c
philtabor <ptabor@gmail.com>
2019-05-08 16:37:21 -0700

    improve model for lunar lander

--- ReinforcementLearning/PolicyGradient/reinforce/reinforce_lunar_lander.py
+++ ReinforcementLearning/PolicyGradient/reinforce/reinforce_lunar_lander.py
@@ -3,26 +3,32 @@ import gym
 from model import PolicyGradientAgent
 import matplotlib.pyplot as plt
 from utils import plotLearning
+from gym import wrappers
 
 if __name__ == '__main__':
-    agent = PolicyGradientAgent(ALPHA=0.001, input_dims=8, n_actions=4)
+    agent = PolicyGradientAgent(ALPHA=0.0005, input_dims=8, GAMMA=0.99,
+                                n_actions=4, layer1_size=64, layer2_size=64,
+                                chkpt_dir='tmp/lunar-lander-ckpt')
+    #agent.load_checkpoint()
     env = gym.make('LunarLander-v2')
     score_history = []
     score = 0
-    num_episodes = 3000
-
+    num_episodes = 2500
+    #env = wrappers.Monitor(env, "tmp/lunar-lander",
+    #                        video_callable=lambda episode_id: True, force=True)
     for i in range(num_episodes):
         print('episode: ', i,'score: ', score)
         done = False
-        score = 0        
+        score = 0
         observation = env.reset()
-        while not done:    
+        while not done:
             action = agent.choose_action(observation)
-            observation_, reward, done, info = env.step(action)                        
-            agent.store_transition(observation, action, reward)            
-            observation = observation_               
+            observation_, reward, done, info = env.step(action)
+            agent.store_transition(observation, action, reward)
+            observation = observation_
             score += reward
-        score_history.append(score)        
-        agent.learn()                
-    filename = 'alpha001-rewards-lunar-lander.png'
-    plotLearning(score_history, filename=filename, window=50)
+        score_history.append(score)
+        agent.learn()
+        #agent.save_checkpoint()
+    #filename = 'lunar-lander-alpha0005-64x64fc-newG.png'
+    #plotLearning(score_history, filename=filename, window=25)
