44446b277b26135d68def3236b8e4d2390123994
philtabor <ptabor@gmail.com>
2019-08-07 16:56:55 -0700

    rename for clarity.

--- ReinforcementLearning/DeepQLearning/main_tf_dqn_breakout.py
+++ ReinforcementLearning/DeepQLearning/main_tf_dqn_breakout.py
@@ -1,3 +1,4 @@
+import os
 import gym
 from dqn_tf import DeepQNetwork, Agent
 from utils import plotLearning
@@ -6,6 +7,7 @@ from gym import wrappers
 import matplotlib.pyplot as plt
 
 def preprocess(observation):
+    observation = observation / 255
     return np.mean(observation[30:,:], axis=2).reshape(180,160,1)
 
 def stack_frames(stacked_frames, frame, buffer_size):
@@ -23,22 +25,26 @@ def stack_frames(stacked_frames, frame, buffer_size):
 
 
 if __name__ == '__main__':
+    #os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
+    #os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+    #os.environ['TF_FORCE_GPU_ALLOW_GROWTH'] = 'true'
+
     env = gym.make('Breakout-v0')
-    load_checkpoint = True
-    agent = Agent(gamma=0.99, epsilon=1.0, alpha=0.00025, input_dims=(180,160,4),
-                  n_actions=3, mem_size=25000, batch_size=32)
+    load_checkpoint = False
+    agent = Agent(gamma=0.99, epsilon=1.0, alpha=0.000025, input_dims=(180,160,4),
+                  n_actions=3, mem_size=25000, batch_size=64)
     if load_checkpoint:
         agent.load_models()
-    filename = 'breakout-alpha0p000025-gamma0p99-only-one-fc.png'
+    filename = 'breakout-alpha0p000025-gamma0p9-only-one-fc-2.png'
     scores = []
     eps_history = []
-    numGames = 5000
+    numGames = 50000
     stack_size = 4
     score = 0
     # uncomment the line below to record every episode.
     #env = wrappers.Monitor(env, "tmp/breakout-0",
     #                         video_callable=lambda episode_id: True, force=True)
-
+    """
     print("Loading up the agent's memory with random gameplay")
 
     while agent.mem_cntr < 25000:
@@ -58,18 +64,14 @@ if __name__ == '__main__':
                                    reward, observation_, int(done))
             observation = observation_
     print("Done with random gameplay. Game on.")
-
+    """
+    n_steps = 0
     for i in range(numGames):
         done = False
-        if i % 10 == 0 and i > 0:
-            avg_score = np.mean(scores[max(0, i-10):(i+1)])
-            print('episode: ', i,'score: ', score,
-                 ' average score %.3f' % avg_score,
-                'epsilon %.3f' % agent.epsilon)
-            agent.save_models()
-        else:
-            print('episode: ', i,'score: ', score)
+        #if i % 100 == 0 and i > 0:
+        #    x = [j+1 for j in range(i)]
 
+        #    plotLearning(x, scores, eps_history, filename)
         observation = env.reset()
         observation = preprocess(observation)
         stacked_frames = None
@@ -79,6 +81,7 @@ if __name__ == '__main__':
             action = agent.choose_action(observation)
             action += 1
             observation_, reward, done, info = env.step(action)
+            n_steps += 1
             observation_ = stack_frames(stacked_frames,
                                         preprocess(observation_), stack_size)
             score += reward
@@ -86,8 +89,16 @@ if __name__ == '__main__':
             agent.store_transition(observation, action,
                                    reward, observation_, int(done))
             observation = observation_
-            agent.learn()
-
+            if n_steps % 4 == 0:
+                agent.learn()
+        if i % 12 == 0 and i > 0:
+            avg_score = np.mean(scores[max(0, i-12):(i+1)])
+            print('episode: ', i,'score: ', score,
+                 ' average score %.3f' % avg_score,
+                'epsilon %.3f' % agent.epsilon)
+            agent.save_models()
+        else:
+            print('episode: ', i,'score: ', score)
         eps_history.append(agent.epsilon)
         scores.append(score)
     x = [i+1 for i in range(numGames)]
