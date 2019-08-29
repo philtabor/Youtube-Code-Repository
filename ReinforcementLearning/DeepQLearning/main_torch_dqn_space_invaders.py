8e7102c38f09e669a79b1402f731615dd5af8f4e
philtabor <ptabor@gmail.com>
2019-08-29 16:16:04 -0700

    rename for clarity

--- ReinforcementLearning/DeepQLearning/main_torch_dqn_space_invaders.py
+++ ReinforcementLearning/DeepQLearning/main_torch_dqn_space_invaders.py
@@ -1,14 +1,14 @@
 import gym
-from deepQModel import DeepQNetwork, Agent
+from torch_deep_q_model import DeepQNetwork, Agent
 from utils import plotLearning
-import numpy as np 
+import numpy as np
 from gym import wrappers
 
 if __name__ == '__main__':
     env = gym.make('SpaceInvaders-v0')
     brain = Agent(gamma=0.95, epsilon=1.0, 
                   alpha=0.003, maxMemorySize=5000,
-                  replace=None)   
+                  replace=None)
     while brain.memCntr < brain.memSize:
         observation = env.reset()
         done = False
@@ -17,8 +17,8 @@ if __name__ == '__main__':
             action = env.action_space.sample()
             observation_, reward, done, info = env.step(action)
             if done and info['ale.lives'] == 0:
-                reward = -100                  
-            brain.storeTransition(np.mean(observation[15:200,30:125], axis=2), action, reward, 
+                reward = -100
+            brain.storeTransition(np.mean(observation[15:200,30:125], axis=2), action, reward,
                                 np.mean(observation_[15:200,30:125], axis=2))
             observation = observation_
     print('done initializing memory')
@@ -27,16 +27,16 @@ if __name__ == '__main__':
     epsHistory = []
     numGames = 50
     batch_size=32
-    # uncomment the line below to record every episode. 
-    # env = wrappers.Monitor(env, "tmp/space-invaders-1", video_callable=lambda episode_id: True, force=True)
+    # uncomment the line below to record every episode.
+    env = wrappers.Monitor(env, "tmp/space-invaders-1", video_callable=lambda episode_id: True, force=True)
     for i in range(numGames):
         print('starting game ', i+1, 'epsilon: %.4f' % brain.EPSILON)
-        epsHistory.append(brain.EPSILON)        
+        epsHistory.append(brain.EPSILON)
         done = False
         observation = env.reset()
         frames = [np.sum(observation[15:200,30:125], axis=2)]
         score = 0
-        lastAction = 0   
+        lastAction = 0
         while not done:
             if len(frames) == 3:
                 action = brain.chooseAction(frames)
@@ -47,10 +47,10 @@ if __name__ == '__main__':
             score += reward
             frames.append(np.sum(observation_[15:200,30:125], axis=2))
             if done and info['ale.lives'] == 0:
-                reward = -100 
-            brain.storeTransition(np.mean(observation[15:200,30:125], axis=2), action, reward, 
+                reward = -100
+            brain.storeTransition(np.mean(observation[15:200,30:125], axis=2), action, reward,
                                   np.mean(observation_[15:200,30:125], axis=2))
-            observation = observation_            
+            observation = observation_
             brain.learn(batch_size)
             lastAction = action
             #env.render(
@@ -58,5 +58,5 @@ if __name__ == '__main__':
         print('score:',score)
     x = [i+1 for i in range(numGames)]
     fileName = str(numGames) + 'Games' + 'Gamma' + str(brain.GAMMA) + \
-               'Alpha' + str(brain.ALPHA) + 'Memory' + str(brain.memSize)+ '.png'    
+               'Alpha' + str(brain.ALPHA) + 'Memory' + str(brain.memSize)+ '.png'
     plotLearning(x, scores, epsHistory, fileName)
