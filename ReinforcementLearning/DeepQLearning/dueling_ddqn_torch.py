33523f669663d15e436a3ae77ea48ab6c7d3a9eb
philtabor <ptabor@gmail.com>
2019-11-26 07:02:25 -0700

    dueling ddqn implementation

---
+++ ReinforcementLearning/DeepQLearning/dueling_ddqn_torch.py
@@ -0,0 +1,170 @@
+import os
+import numpy as np
+import torch as T
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+
+class ReplayBuffer():
+    def __init__(self, max_size, input_shape):
+        self.mem_size = max_size
+        self.mem_cntr = 0
+        self.state_memory = np.zeros((self.mem_size, *input_shape),
+                                    dtype=np.float32)
+        self.new_state_memory = np.zeros((self.mem_size, *input_shape),
+                                        dtype=np.float32)
+        self.action_memory = np.zeros(self.mem_size, dtype=np.int64)
+        self.reward_memory = np.zeros(self.mem_size, dtype=np.float32)
+        self.terminal_memory = np.zeros(self.mem_size, dtype=np.uint8)
+
+    def store_transition(self, state, action, reward, state_, done):
+        index = self.mem_cntr % self.mem_size
+        self.state_memory[index] = state
+        self.new_state_memory[index] = state_
+        self.action_memory[index] = action
+        self.reward_memory[index] = reward
+        self.terminal_memory[index] = done
+        self.mem_cntr += 1
+
+    def sample_buffer(self, batch_size):
+        max_mem = min(self.mem_cntr, self.mem_size)
+        batch = np.random.choice(max_mem, batch_size, replace=False)
+
+        states = self.state_memory[batch]
+        actions = self.action_memory[batch]
+        rewards = self.reward_memory[batch]
+        states_ = self.new_state_memory[batch]
+        terminal = self.terminal_memory[batch]
+
+        return states, actions, rewards, states_, terminal
+
+class DuelingDeepQNetwork(nn.Module):
+    def __init__(self, lr, n_actions, name, input_dims, chkpt_dir):
+        super(DuelingDeepQNetwork, self).__init__()
+        self.checkpoint_dir = chkpt_dir
+        self.checkpoint_file = os.path.join(self.checkpoint_dir, name)
+
+        self.fc1 = nn.Linear(*input_dims, 512)
+        self.V = nn.Linear(512, 1)
+        self.A = nn.Linear(512, n_actions)
+
+        self.optimizer = optim.Adam(self.parameters(), lr=lr)
+        self.loss = nn.MSELoss()
+        self.device = T.device('cuda:0' if T.cuda.is_available() else 'cpu')
+        self.to(self.device)
+
+    def forward(self, state):
+        flat1 = F.relu(self.fc1(state))
+        V = self.V(flat1)
+        A = self.A(flat1)
+
+        return V, A
+
+    def save_checkpoint(self):
+        print('... saving checkpoint ...')
+        T.save(self.state_dict(), self.checkpoint_file)
+
+    def load_checkpoint(self):
+        print('... loading checkpoint ...')
+        self.load_state_dict(T.load(self.checkpoint_file))
+
+class Agent():
+    def __init__(self, gamma, epsilon, lr, n_actions, input_dims,
+                 mem_size, batch_size, eps_min=0.01, eps_dec=5e-7,
+                 replace=1000, chkpt_dir='tmp/dueling_ddqn'):
+        self.gamma = gamma
+        self.epsilon = epsilon
+        self.lr = lr
+        self.n_actions = n_actions
+        self.input_dims = input_dims
+        self.batch_size = batch_size
+        self.eps_min = eps_min
+        self.eps_dec = eps_dec
+        self.replace_target_cnt = replace
+        self.chkpt_dir = chkpt_dir
+        self.action_space = [i for i in range(self.n_actions)]
+        self.learn_step_counter = 0
+
+        self.memory = ReplayBuffer(mem_size, input_dims, n_actions)
+
+        self.q_eval = DuelingDeepQNetwork(self.lr, self.n_actions,
+                                   input_dims=self.input_dims,
+                                   name='lunar_lander_dueling_ddqn_q_eval',
+                                   chkpt_dir=self.chkpt_dir)
+
+        self.q_next = DuelingDeepQNetwork(self.lr, self.n_actions,
+                                   input_dims=self.input_dims,
+                                   name='lunar_lander_dueling_ddqn_q_next',
+                                   chkpt_dir=self.chkpt_dir)
+
+    def choose_action(self, observation):
+        if np.random.random() > self.epsilon:
+            state = T.tensor([observation],dtype=T.float).to(self.q_eval.device)
+            _, advantage = self.q_eval.forward(state)
+            action = T.argmax(advantage).item()
+        else:
+            action = np.random.choice(self.action_space)
+
+        return action
+
+    def store_transition(self, state, action, reward, state_, done):
+        self.memory.store_transition(state, action, reward, state_, done)
+
+    def replace_target_network(self):
+        if self.learn_step_counter % self.replace_target_cnt == 0:
+            self.q_next.load_state_dict(self.q_eval.state_dict())
+
+    def decrement_epsilon(self):
+        self.epsilon = self.epsilon - self.eps_dec \
+                        if self.epsilon > self.eps_min else self.eps_min
+
+    def save_models(self):
+        self.q_eval.save_checkpoint()
+        self.q_next.save_checkpoint()
+
+    def load_models(self):
+        self.q_eval.load_checkpoint()
+        self.q_next.load_checkpoint()
+
+    def learn(self):
+        if self.memory.mem_cntr < self.batch_size:
+            return
+
+        self.q_eval.optimizer.zero_grad()
+
+        self.replace_target_network()
+
+        state, action, reward, new_state, done = \
+                                self.memory.sample_buffer(self.batch_size)
+
+        states = T.tensor(state).to(self.q_eval.device)
+        rewards = T.tensor(reward).to(self.q_eval.device)
+        dones = T.tensor(done).to(self.q_eval.device)
+        actions = T.tensor(action).to(self.q_eval.device)
+        states_ = T.tensor(new_state).to(self.q_eval.device)
+
+        indices = np.arange(self.batch_size)
+
+        V_s, A_s = self.q_eval.forward(states)
+        V_s_, A_s_ = self.q_next.forward(states_)
+
+        V_s_eval, A_s_eval = self.q_eval.forward(states_)
+
+        q_pred = T.add(V_s,
+                        (A_s - A_s.mean(dim=1, keepdim=True)))[indices, actions]
+        q_next = T.add(V_s_,
+                        (A_s_ - A_s_.mean(dim=1, keepdim=True)))
+
+        q_eval = T.add(V_s_eval, (A_s_eval - A_s_eval.mean(dim=1,keepdim=True)))
+
+        max_actions = T.argmax(q_eval, dim=1)
+
+        q_next[dones] = 0.0
+        q_target = rewards + self.gamma*q_next[indices, max_actions]
+
+        loss = self.q_eval.loss(q_target, q_pred).to(self.q_eval.device)
+        loss.backward()
+        self.q_eval.optimizer.step()
+        self.learn_step_counter += 1
+
+        self.decrement_epsilon()
---
+++ ReinforcementLearning/DeepQLearning/main_torch_dueling_ddqn_lunar_lander.py
@@ -0,0 +1,49 @@
+import gym
+import numpy as np
+from dueling_ddqn_torch import Agent
+from utils import plotLearning
+
+if __name__ == '__main__':
+    env = gym.make('LunarLander-v2')
+    num_games = 250
+    load_checkpoint = False
+
+    agent = Agent(gamma=0.99, epsilon=1.0, lr=5e-4,
+                  input_dims=[8], n_actions=4, mem_size=100000, eps_min=0.01,
+                  batch_size=64, eps_dec=1e-3, replace=100)
+
+    if load_checkpoint:
+        agent.load_models()
+
+    filename = 'LunarLander-Dueling-DDQN-512-Adam-lr0005-replace100.png'
+    scores = []
+    eps_history = []
+    n_steps = 0
+
+    for i in range(num_games):
+        done = False
+        observation = env.reset()
+        score = 0
+
+        while not done:
+            action = agent.choose_action(observation)
+            observation_, reward, done, info = env.step(action)
+            score += reward
+            agent.store_transition(observation, action,
+                                    reward, observation_, int(done))
+            agent.learn()
+
+            observation = observation_
+
+        scores.append(score)
+        avg_score = np.mean(scores[max(0, i-100):(i+1)])
+        print('episode: ', i,'score %.1f ' % score,
+             ' average score %.1f' % avg_score,
+            'epsilon %.2f' % agent.epsilon)
+        if i > 0 and i % 10 == 0:
+            agent.save_models()
+
+        eps_history.append(agent.epsilon)
+
+    x = [i+1 for i in range(num_games)]
+    plotLearning(x, scores, eps_history, filename)
