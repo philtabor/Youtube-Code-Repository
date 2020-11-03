338a271830c9e411995c63ec6bb0b9d08b3a3e2a
philtabor <ptabor@gmail.com>
2020-11-03 16:50:03 -0700

    add replace=False

--- ReinforcementLearning/PolicyGradient/DDPG/tensorflow2/pendulum/buffer.py
+++ ReinforcementLearning/PolicyGradient/DDPG/tensorflow2/pendulum/buffer.py
@@ -24,7 +24,7 @@ class ReplayBuffer:
     def sample_buffer(self, batch_size):
         max_mem = min(self.mem_cntr, self.mem_size)
 
-        batch = np.random.choice(max_mem, batch_size)
+        batch = np.random.choice(max_mem, batch_size, replace=False)
 
         states = self.state_memory[batch]
         states_ = self.new_state_memory[batch]
