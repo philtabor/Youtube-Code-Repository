ff408a89fd94937148c02f913c8beb41b475133f
philtabor <ptabor@gmail.com>
2021-10-15 10:50:54 -0700

    code cleanup

--- ReinforcementLearning/DeepQLearning/simple_dqn_torch_2020.py
+++ ReinforcementLearning/DeepQLearning/simple_dqn_torch_2020.py
@@ -4,9 +4,10 @@ import torch.nn.functional as F
 import torch.optim as optim
 import numpy as np
 
+
 class DeepQNetwork(nn.Module):
-    def __init__(self, lr, input_dims, fc1_dims, fc2_dims, 
-            n_actions):
+    def __init__(self, lr, input_dims, fc1_dims, fc2_dims,
+                 n_actions):
         super(DeepQNetwork, self).__init__()
         self.input_dims = input_dims
         self.fc1_dims = fc1_dims
@@ -28,9 +29,10 @@ class DeepQNetwork(nn.Module):
 
         return actions
 
-class Agent():
+
+class Agent:
     def __init__(self, gamma, epsilon, lr, input_dims, batch_size, n_actions,
-            max_mem_size=100000, eps_end=0.05, eps_dec=5e-4):
+                 max_mem_size=100000, eps_end=0.05, eps_dec=5e-4):
         self.gamma = gamma
         self.epsilon = epsilon
         self.eps_min = eps_end
@@ -43,13 +45,13 @@ class Agent():
         self.iter_cntr = 0
         self.replace_target = 100
 
-        self.Q_eval = DeepQNetwork(lr, n_actions=n_actions, input_dims=input_dims,
-                                    fc1_dims=256, fc2_dims=256)
-        self.Q_next = DeepQNetwork(lr, n_actions=n_actions, input_dims=input_dims,
-                                    fc1_dims=64, fc2_dims=64)
-
-        self.state_memory = np.zeros((self.mem_size, *input_dims), dtype=np.float32)
-        self.new_state_memory = np.zeros((self.mem_size, *input_dims), dtype=np.float32)
+        self.Q_eval = DeepQNetwork(lr, n_actions=n_actions,
+                                   input_dims=input_dims,
+                                   fc1_dims=256, fc2_dims=256)
+        self.state_memory = np.zeros((self.mem_size, *input_dims),
+                                     dtype=np.float32)
+        self.new_state_memory = np.zeros((self.mem_size, *input_dims),
+                                         dtype=np.float32)
         self.action_memory = np.zeros(self.mem_size, dtype=np.int32)
         self.reward_memory = np.zeros(self.mem_size, dtype=np.float32)
         self.terminal_memory = np.zeros(self.mem_size, dtype=np.bool)
@@ -79,35 +81,31 @@ class Agent():
             return
 
         self.Q_eval.optimizer.zero_grad()
-        
+
         max_mem = min(self.mem_cntr, self.mem_size)
 
         batch = np.random.choice(max_mem, self.batch_size, replace=False)
-        
         batch_index = np.arange(self.batch_size, dtype=np.int32)
 
         state_batch = T.tensor(self.state_memory[batch]).to(self.Q_eval.device)
-        new_state_batch = T.tensor(self.new_state_memory[batch]).to(self.Q_eval.device)
+        new_state_batch = T.tensor(
+                self.new_state_memory[batch]).to(self.Q_eval.device)
         action_batch = self.action_memory[batch]
-        reward_batch = T.tensor(self.reward_memory[batch]).to(self.Q_eval.device)
-        terminal_batch = T.tensor(self.terminal_memory[batch]).to(self.Q_eval.device)
+        reward_batch = T.tensor(
+                self.reward_memory[batch]).to(self.Q_eval.device)
+        terminal_batch = T.tensor(
+                self.terminal_memory[batch]).to(self.Q_eval.device)
 
         q_eval = self.Q_eval.forward(state_batch)[batch_index, action_batch]
         q_next = self.Q_eval.forward(new_state_batch)
         q_next[terminal_batch] = 0.0
 
-        q_target = reward_batch + self.gamma*T.max(q_next,dim=1)[0]
+        q_target = reward_batch + self.gamma*T.max(q_next, dim=1)[0]
 
         loss = self.Q_eval.loss(q_target, q_eval).to(self.Q_eval.device)
         loss.backward()
         self.Q_eval.optimizer.step()
 
         self.iter_cntr += 1
-        self.epsilon = self.epsilon - self.eps_dec if self.epsilon > self.eps_min \
-                       else self.eps_min
-
-        #if self.iter_cntr % self.replace_target == 0:
-        #   self.Q_next.load_state_dict(self.Q_eval.state_dict())
-
-
-
+        self.epsilon = self.epsilon - self.eps_dec \
+            if self.epsilon > self.eps_min else self.eps_min
